f7b42bf683ed0e9ce0fdfb61d526f8749bee69fd
prkr-28 <priyanshu402muz@gmail.com>
2025-10-19 07:45:33 +0530

    dashboard my-pages and sharedpages section upgrade

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -36,11 +36,12 @@ const config = {
       : process.env.ALLOWED_ORIGINS
         ? process.env.ALLOWED_ORIGINS.split(',')
         : [
-            'http://localhost:3000',
+            'http://localhost:5173',
             'http://localhost:3001',
             'http://localhost:3002',
             'http://localhost:80',
-            'http://localhost:5173',
+            'http://localhost:3000',
+            'http://localhost:5173/',
           ],
   },
 
