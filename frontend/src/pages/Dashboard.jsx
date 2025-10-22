6bfd3baf3ee71342d23696f0e46b8cedd1701590
MannuVilasara <mannuvilasara@gmail.com>
2025-10-22 17:16:24 +0530

    fix: wrong import parth

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import TopBar from '../components/dashboard/TopBar';
 import Note from '../components/dashboard/Note';
 import Reminder from '../components/dashboard/Reminder';
 import authContext from '../context/AuthProvider';
-import { usePageCache } from '../context/usePageCache';
+import { usePageCache } from '../hooks/usePageCache.js';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useNavigate, useLocation } from 'react-router-dom';
