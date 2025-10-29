769d549b9e090485d8ce61d12f2c20e4d4dfa66d
Bhavani Saw <sawbhavani31@gmail.com>
2025-10-30 00:04:25 +0530

    Fix: mobile misalignment issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // allow other devices (like your phone) to access it
+    port: 5173, // default Vite port
+  },
 });
